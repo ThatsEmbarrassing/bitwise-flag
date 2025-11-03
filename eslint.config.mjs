b260d2562d3ab46487c7157098ff4d559c657c6b
EmberIsReal <thatsembarrassing@proton.me>
2025-11-03 14:12:40 +0300

    style: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import eslint from "@eslint/js";
+
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+import stylistic from "@stylistic/eslint-plugin";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/always-multiline": ["error", "always-multiline"],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { varsIgnorePattern: "_", argsIgnorePattern: "_" },
+      ],
+    },
+  },
+  globalIgnores(["dist/**/*"], "Ignore build directory"),
+  globalIgnores(["**/*.test.*"], "Ignore test files")
+);
