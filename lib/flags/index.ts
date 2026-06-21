32c91ad2068190588c2e36736f42ec2e7af3aeb5
Ember <github.omit833@passinbox.com>
2026-06-22 02:01:06 +0300

    feat(flags): append utils re-export

--- lib/flags/index.ts
+++ lib/flags/index.ts
@@ -3,5 +3,6 @@ export { BigIntFlagRegistry, NumberFlagRegistry } from "./registry";
 export { Repository } from "./repository";
 
 export * from "./errors";
+export * from "./utils";
 
 export type * from "./types";
